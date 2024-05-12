8a268b3617ca1152ca987ae7f202f685a9ce662f
Guilherme Brito <gui.brito00@gmail.com>
2024-05-12 18:42:31 -0300

    Teste SmartHint

--- TesteSmartHintApplication/obj/Debug/net8.0/TesteSmartHint.Application.AssemblyInfo.cs
+++ TesteSmartHintApplication/obj/Debug/net8.0/TesteSmartHint.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteSmartHint.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a08955e3f0b03ca99e97666f91056a6f7ab34c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteSmartHint.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteSmartHint.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TesteSmartHintInfrastructure/obj/Debug/net8.0/TesteSmartHint.Infrastructure.AssemblyInfo.cs
+++ TesteSmartHintInfrastructure/obj/Debug/net8.0/TesteSmartHint.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteSmartHint.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a08955e3f0b03ca99e97666f91056a6f7ab34c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteSmartHint.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteSmartHint.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
