9d96a6909a28e65b630e41832afbe289db7451a2
Guilherme Brito <gui.brito00@gmail.com>
2024-05-14 18:54:42 -0300

    Create WebApplication

---
+++ TesteSmartHint.WebApplication/TesteSmartHint.WebApplication/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TesteSmartHint.WebApplication.Startup))]
+namespace TesteSmartHint.WebApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
